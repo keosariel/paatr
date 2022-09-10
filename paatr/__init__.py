c883d288007274e69becc059f4c69e8293dfbdd1
keosariel <keosariel@icloud.com>
2022-09-10 15:59:14 +0100

    impl: file uploads

--- paatr/__init__.py
+++ paatr/__init__.py
@@ -1,5 +1,11 @@
+import os
+
 from dotenv import dotenv_values
 from supabase import create_client, Client
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+_APP_FILES_DIR = os.path.join(BASE_DIR, "__apps_data__")
+APP_FILES_DIR = os.path.join(_APP_FILES_DIR, "apps")
 
 ENV = dotenv_values(".env")  # take environment variables from .env.
 supabase: Client = create_client(ENV['SUPABASE_URL'], ENV['SUPABASE_KEY'])
--- paatr/helpers.py
+++ paatr/helpers.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import Request
 from fastapi.responses import JSONResponse
 
@@ -7,3 +9,15 @@ async def handle_errors(request: Request, exc: Exception):
         status_code=500,
         content={"message": str(exc)},
     )
+
+async def save_file(filename, dir_path, contents, _mode="wb"):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    try:
+        with open(os.path.join(dir_path, filename), _mode) as fp:
+            fp.write(contents)
+    except Exception as e:
+        return False
+    
+    return True
