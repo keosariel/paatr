37e01961b4d6bd807dfc771a287a829e563050e2
keosariel <keosariel@icloud.com>
2022-09-10 18:14:03 +0100

    impl: generate dockerfile && paatr config

--- paatr/__init__.py
+++ paatr/__init__.py
@@ -4,8 +4,32 @@ from dotenv import dotenv_values
 from supabase import create_client, Client
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-_APP_FILES_DIR = os.path.join(BASE_DIR, "__apps_data__")
-APP_FILES_DIR = os.path.join(_APP_FILES_DIR, "apps")
+APP_FILES_DIR = os.path.join(BASE_DIR, "__apps_data__")
+APP_FILES_DIR = os.path.join(APP_FILES_DIR, "apps")
+APP_CONFIG_FILE = "paatr.yaml"
 
 ENV = dotenv_values(".env")  # take environment variables from .env.
 supabase: Client = create_client(ENV['SUPABASE_URL'], ENV['SUPABASE_KEY'])
+
+# TODO: Add python versions
+PYTHON_VERSION_DOCKER_MAPS = {}
+
+CONFIG_KEYS_X = ["runtime", "run", "port", "start"]
+CONFIG_KEYS = CONFIG_KEYS_X + ["env"]
+
+CONFIG_VALUE_VALIDATOR = {
+    "runtime": lambda x: type(x) == str,
+    "run": lambda x: type(x) == str or type(x) == list,
+    "port": lambda x: type(x) == int,
+    "start": lambda x: type(x) == str,
+    "env": lambda x: type(x) == dict
+}
+
+DOCKER_TEMPLATE = """
+FROM python:{runtime}
+WORKDIR /app
+COPY . .
+{run}
+EXPOSE {port}
+CMD {start}
+"""
--- paatr/helpers.py
+++ paatr/helpers.py
@@ -1,8 +1,13 @@
 import os
+import tempfile
+import zipfile
+import yaml
 
 from fastapi import Request
 from fastapi.responses import JSONResponse
 
+from . import (APP_CONFIG_FILE, CONFIG_KEYS_X, CONFIG_KEYS, 
+                CONFIG_VALUE_VALIDATOR, DOCKER_TEMPLATE)
 
 async def handle_errors(request: Request, exc: Exception):
     return JSONResponse(
@@ -21,3 +26,63 @@ async def save_file(filename, dir_path, contents, _mode="wb"):
         return False
     
     return True
+
+async def get_app_config(config_path):
+    with open(config_path, "r") as fp:
+        config = yaml.safe_load(fp)
+    
+    if not config:
+        return False, f"Invalid {APP_CONFIG_FILE} file"
+    
+    for key in CONFIG_KEYS_X:
+        if key not in config:
+            return False, f"Missing {key} key in {APP_CONFIG_FILE}"
+
+        if not CONFIG_VALUE_VALIDATOR[key](config[key]):
+            return False, f"Invalid value for {key} key in {APP_CONFIG_FILE}"
+
+    for k,v in config.items():
+        if k not in CONFIG_KEYS:
+            return False, f"Invalid key {k} in {APP_CONFIG_FILE}"
+        
+        if k in CONFIG_KEYS_X:
+            if not v:
+                return False, f"Invalid value for {k} in {APP_CONFIG_FILE}"
+
+    return True, config
+
+def generate_docker_config(config):
+    run = config.pop("run")
+    if type(run) == list:
+        run = " && ".join(run)
+
+    return DOCKER_TEMPLATE.format(**config, run=f"RUN {run}")
+
+async def build_app(app_path):
+    if not os.path.exists(app_path):
+        return "No files found for this app"
+    
+    try:
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            with zipfile.ZipFile(app_path, "r") as zip_ref:
+                zip_ref.extractall(tmp_dir)
+
+                files = os.listdir(tmp_dir)
+                
+                if APP_CONFIG_FILE not in files:
+                    return f"No {APP_CONFIG_FILE} file found in the app files"
+
+                (is_valid, config) = await get_app_config(os.path.join(tmp_dir, APP_CONFIG_FILE))
+
+                if not is_valid:
+                    return config
+                
+                dockerfile = generate_docker_config(config)
+
+                with open(os.path.join(tmp_dir, "dockerfile"), "w") as fp:
+                    fp.write(dockerfile)
+                
+    except Exception as e:
+        return "Failed to build app"
+    
+    return "App built successfully"
